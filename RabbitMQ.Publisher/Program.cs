e4d2fed14290237963327d43f7a4872ebbe63460
Mehmet Dokuyucu <50601213+mehmetdkyc@users.noreply.github.com>
2023-04-28 01:22:42 +0300

    Exchange Types learned.

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -7,14 +7,20 @@ var connectionString = "amqps://tnywxfoe:2zNoaSpp5PH7xkHY-v6o9GqIa6yPPM-E@moose.
 var connectionStringDocker = "amqp://localhost:5672";
 //Bağlantıyı Oluşturma
 ConnectionFactory factory = new();
-factory.Uri = new (connectionStringDocker);
+factory.Uri = new(connectionStringDocker);
 
 //Bağlantıyı Aktifleştirme ve Kanal Açma
-using IConnection connection =  factory.CreateConnection(); //bağlantıyı aktifleştirdik.
-using IModel channel =  connection.CreateModel(); // kanalı açtık.
+using IConnection connection = factory.CreateConnection(); //bağlantıyı aktifleştirdik.
+using IModel channel = connection.CreateModel(); // kanalı açtık.
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true; //mesajları kalıcı hale getirme
+/// <summary>
+/// Yukarısı fix aynıdır genelde.
+/// </summary>
 
 //Queue Oluşturma
-channel.QueueDeclare(queue:"example-queue", exclusive:false,durable:true, autoDelete:false); // exclusive false olması demek birden fazla channel tarafından bu kuyruğun üzerinde işlem yapılabilecek demektir.
+//channel.QueueDeclare(queue:"example-queue", exclusive:false,durable:true, autoDelete:false); // exclusive false olması demek birden fazla channel tarafından bu kuyruğun üzerinde işlem yapılabilecek demektir.
 //durable özelliğini true yapmamız bu kuyruğun kalıcı olmasını tanımladığını anlamına gelmektedir.Consumer tarafında da bu alan true olarak verilmelidir.
 //autodelete: kuyruğa bağlı consumerlar eğer olmazsa otomatik olarak sil demek.
 
@@ -28,16 +34,85 @@ channel.QueueDeclare(queue:"example-queue", exclusive:false,durable:true, autoDe
 //channel.BasicPublish(exchange:"", routingKey: "example-queue", body: messageExample); //default exchange direct exchange demektir. Şimdilik boş geçiyoruz, default kabul ediyoruz.
 //routing-keydeki queue ismidir, yukarıda 17.satırda oluşturduğumuz kuyruğumuzdur.
 
-IBasicProperties properties= channel.CreateBasicProperties();
-properties.Persistent = true;
-//bu properties instance'ı kuyrukta mesajlar için kalıcılık özelliğini tanımlamamızdır. Aşağıda basicpublish tarafında setleme işlemi yaparak kalıcı hale getiriyoruz.
-//örnek mesela
 
-for (int i = 0; i < 100; i++)
+
+#region Direct Exchange
+//var exchangeDirectName = "logs-direct-exchange";
+
+//var queuename = "direct-queue-infologs";
+//var routingKey = "route-direct-log";
+
+//channel.ExchangeDeclare(exchange: exchangeDirectName, durable:true,type:ExchangeType.Direct); //direct exchange tanımlama
+
+//channel.QueueDeclare(queue:queuename,durable:true,autoDelete:false,exclusive:false); // queue'muzu oluşturma
+
+//channel.QueueBind(queue:queuename,exchange: exchangeDirectName, routingKey: routingKey, null); //ilgili kuyruğumuzu exchange'e bind etme.
+
+
+////bu properties instance'ı kuyrukta mesajlar için kalıcılık özelliğini tanımlamamızdır. Aşağıda basicpublish tarafında setleme işlemi yaparak kalıcı hale getiriyoruz.
+
+//for (int i = 0; i < 5; i++)
+//{
+//    await Task.Delay(200);
+//    byte[] messageExamples = Encoding.UTF8.GetBytes($"Direct log : {i}");
+//    channel.BasicPublish(exchange: exchangeDirectName, routingKey: routingKey, body: messageExamples, basicProperties: properties);
+//}
+//// ayağa kalktığında https://moose.rmq.cloudamqp.com/#/connections sitesinde connect olduğunu görebileceğiz.
+//Console.Read();
+#endregion
+
+
+
+#region Topic exchange 
+//var exchangeTopicName = "logs-topic";
+//var queueNameTopic = "topicQueue";
+//var routingKeyTopic = "*.Error.*";
+
+//channel.ExchangeDeclare(exchange: exchangeTopicName, durable: true, type: ExchangeType.Topic);
+//channel.QueueDeclare(queue: queueNameTopic, durable: true, autoDelete: false, exclusive: false); // queue'muzu oluşturma
+
+//channel.QueueBind(queue: queueNameTopic, exchange: exchangeTopicName, routingKey: routingKeyTopic, null);
+//Random rnd = new();
+//for (int i = 0; i < 50; i++)
+//{
+//    // "Critical.Info.Normal"; örnek bir mesajın routeKey
+//    LogNames log1 = (LogNames)rnd.Next(1, 5);
+//    LogNames log2 = (LogNames)rnd.Next(1, 5);
+//    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+//    var routeTopicKey = $"{log1}.{log2}.{log3}";
+//    var messageTopic = $"Topic log-type : {log1}-{log2}-{log3}";
+//    byte[] messageTopicExamples = Encoding.UTF8.GetBytes(messageTopic);
+//    channel.BasicPublish(exchange: exchangeTopicName, routingKey: routeTopicKey, body: messageTopicExamples, basicProperties: properties);
+//    Console.WriteLine($"Mesaj gönderilmiştir: {messageTopic}");
+
+//}
+//Console.Read();
+
+#endregion
+
+#region Header exchange
+
+var exchangeHeaderName = "header-exchange";
+channel.ExchangeDeclare(exchange: exchangeHeaderName, durable: true, type: ExchangeType.Headers);
+
+Dictionary<string,object> headers = new Dictionary<string, object>();
+headers.Add("format","pdf");
+headers.Add("shape1","a4");
+properties.Headers = headers;
+
+byte[] messageHeaderExamples = Encoding.UTF8.GetBytes("Header örnek mesajım..");
+channel.BasicPublish(exchange: exchangeHeaderName, routingKey: String.Empty, body: messageHeaderExamples, basicProperties: properties);
+Console.WriteLine($"Mesaj gönderilmiştir.");
+
+Console.Read();
+
+#endregion
+public enum LogNames
 {
-    await Task.Delay(200);
-    byte[] messageExample100 = Encoding.UTF8.GetBytes($"Merhaba: {i}");
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: messageExample100, basicProperties: properties);
+    Critical = 1,
+    Info = 2,
+    Warning = 3,
+    Error = 4,
+    Normal = 5
 }
-
-Console.Read(); // ayağa kalktığında https://moose.rmq.cloudamqp.com/#/connections sitesinde connect olduğunu görebileceğiz.
--- RabbitMQ.UdemyCourse.Consumer/Program.cs
+++ RabbitMQ.UdemyCourse.Consumer/Program.cs
@@ -2,7 +2,7 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-var connectionString = "amqp://localhost:5672";
+var connectionString = "amqp://localhost:5672"; //dockerdaki localhost bağlantısı rabbitmq
 //Bağlantıyı Oluşturma
 ConnectionFactory factory = new();
 factory.Uri = new(connectionString);
@@ -16,12 +16,77 @@ channel.BasicQos(prefetchSize:0, prefetchCount:1,global:false); //global false o
 
 var consumer = new EventingBasicConsumer(channel); //consumer oluşturduk 
 
-channel.BasicConsume(queue: "example-queue",autoAck:false,consumer);
-//autoAck false olması: kuyruktan eğer doğru işlenmesi durumunda haber verecek ve mesajı silecek, true olması işlem bittikten sonra doğru mu yanlış mı olmadan direkt silmesidir.
+
+
+#region Direct exchange consumer
+//var exchangeDirectName = "logs-direct-exchange";
+
+//var queuename = "direct-queue-infologs";
+//var routingKey = "route-direct-log";
+
+//channel.BasicConsume(queue: queuename, autoAck: false, consumer);
+////autoAck false olması: kuyruktan eğer doğru işlenmesi durumunda haber verecek ve mesajı silecek, true olması işlem bittikten sonra doğru mu yanlış mı olmadan direkt silmesidir.
+
+//consumer.Received += (sender, e) =>
+//{
+//    Thread.Sleep(1500);
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//    channel.BasicAck(e.DeliveryTag, false); // consumer mesajı başarılı aldığı için rabbitmqye bu mesajı silebilirsin diye bilgi veriyor.
+//};
+//Console.Read();
+
+#endregion
+
+#region Topic exchange cunsomer
+//var exchangeTopicName = "logs-topic"; //istersek her iki taraftada queue ve exchangei oluşturabiliriz ama sadece publisher kısmında oluşturdum. Eğer aynı adta var ise yenisi oluşmuyor.
+
+//var queueNameTopic = "topicQueue"; 
+//var routingKey = "*.Error.*"; //topic route key için istediğimiz route key.
+
+////publisher tarafında ilgili exchange ve kuyruğu oluşturup bind ettik
+////ama garanti olması için burda da oluşturuyoruz eğer ki böyle bir kuyruk veuya exchange var ise önemlideğil oluşturmaz
+
+//channel.ExchangeDeclare(exchange: exchangeTopicName, durable: true, type: ExchangeType.Topic);
+//channel.QueueDeclare(queue: queueNameTopic, durable: true, autoDelete: false, exclusive: false); // queue'muzu oluşturma
+
+//channel.QueueBind(queue: queueNameTopic, exchange: exchangeTopicName, routingKey: routingKey, null);
+
+//channel.BasicConsume(queue: queueNameTopic, autoAck: false, consumer);
+//Console.WriteLine("Loglar dinleniyor..");
+
+//consumer.Received += (sender, e) =>
+//{
+//    Thread.Sleep(1500);
+//    Console.WriteLine("Gelen mesaj: " +  Encoding.UTF8.GetString(e.Body.Span));
+//    channel.BasicAck(e.DeliveryTag, false); // consumer mesajı başarılı aldığı için rabbitmqye bu mesajı silebilirsin diye bilgi veriyor.
+//};
+//Console.Read();
+#endregion
+
+#region Header Exchange
+
+var exchangeHeaderName = "header-exchange";
+var queueNameHeader = "headerQueue";
+
+channel.ExchangeDeclare(exchange: exchangeHeaderName, durable: true, type: ExchangeType.Headers);
+channel.QueueDeclare(queue: queueNameHeader, durable: true, autoDelete: false, exclusive: false); // queue'muzu oluşturma
+
+Dictionary<string, object> headers = new Dictionary<string, object>();
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+headers.Add("x-match", "any"); // yukarıdaki key valuelardan birisini sağlar ise bu kuyrukta mesajları al demek. All olsaydı tüm key value türüne uyan mesajları işleme alacaktı.
+
+channel.QueueBind(queue: queueNameHeader, exchange: exchangeHeaderName, routingKey: String.Empty, headers); // burda headerımızı queueya bind ediyoruz.
+
+channel.BasicConsume(queue: queueNameHeader, autoAck: false, consumer);
+Console.WriteLine("Loglar dinleniyor..");
 
 consumer.Received += (sender, e) =>
 {
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-    channel.BasicAck(e.DeliveryTag,false); // consumer mesajı başarılı aldığı için rabbitmqye bu mesajı silebilirsin diye bilgi veriyor.
+    Thread.Sleep(1500);
+    Console.WriteLine("Gelen mesaj: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+    channel.BasicAck(e.DeliveryTag, false); // consumer mesajı başarılı aldığı için rabbitmqye bu mesajı silebilirsin diye bilgi veriyor.
 };
 Console.Read();
+
+#endregion
